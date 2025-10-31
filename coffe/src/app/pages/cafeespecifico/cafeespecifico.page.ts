a29d7b26fc661b7b8b036fe49272176673ab3b2b
gabriel <jgsobralnunes@gmail.com>
2025-10-31 18:22:00 -0300

    conexao com a api mas nao esta com o local storage

--- coffe/src/app/pages/cafeespecifico/cafeespecifico.page.ts
+++ coffe/src/app/pages/cafeespecifico/cafeespecifico.page.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { cartOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-cafeespecifico',
   templateUrl: './cafeespecifico.page.html',
   styleUrls: ['./cafeespecifico.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, HttpClientModule]
 })
-export class CafeespecificoPage {
-  constructor() {
-       addIcons({cartOutline}); }
+export class CafeespecificoPage implements OnInit {
+  drinks: any[] = [];
+  drinkSelecionado: any = null;
+
+  constructor(private http: HttpClient) {
+    addIcons({ cartOutline });
   }
 
+  ngOnInit() {
+    // Buscar drinks da API (exemplo: categoria "Cocktail")
+    this.http.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
+      .subscribe({
+        next: (res: any) => {
+          // A API retorna { drinks: [...] }
+          this.drinks = res.drinks.map((drink: any, index: number) => ({
+            ...drink,
+            preco: (10 + index * 2).toFixed(2) // preço fictício
+          }));
+        },
+        error: (err) => console.error('Erro ao consumir API:', err)
+      });
+  }
 
-
+  selecionarDrink(drink: any) {
+    this.drinkSelecionado = drink;
+    console.log('Drink selecionado:', drink);
+  }
+}
 
