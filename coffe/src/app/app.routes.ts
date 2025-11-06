a9205c10c86d06947316fe3639c2a51e1186728c
GiovannaCMa <giovannacmarchesincc@gmail.com>
2025-11-06 15:36:11 -0300

    salvando alterações locais antes do pull

--- coffe/src/app/app.routes.ts
+++ coffe/src/app/app.routes.ts
@@ -7,7 +7,7 @@ export const routes: Routes = [
   },
   {
     path: 'cafeespecifico',
-    loadComponent: () => import('./pages/Cafeespecifico/Cafeespecifico.page').then(m => m.CafeespecificoPage)
+    loadComponent: () => import('./pages/cafeespecifico/cafeespecifico.page').then(m => m.CafeespecificoPage)
   },
   {
     path: 'bebidasFrias',
@@ -24,6 +24,11 @@ export const routes: Routes = [
   {
     path: 'cafedetalhes',
     loadComponent: () =>
-      import('./pages/Cafeespecifico/cafeDetalhe/cafeDetalhe.page').then(m => m.CafeDetalhePage )
-  }
+      import('./pages/cafeespecifico/cafeDetalhe/cafeDetalhe.page').then(m => m.CafeDetalhePage )
+  },
+  {
+  path: 'finalizarCompras',
+  loadComponent: () =>
+    import('./pages/finalizarCompras/finalizar.page').then(m => m.FinalizarPedidoPage)
+},
 ];
